95f82c8af80d003d6125e8d1c246fe152500496c
Rishi Banerjee <rishieric91@gmail.com>
2019-05-20 03:18:19 +0530

    Some minor changes'

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# summerofcode
+# Summer Of Algorithm
+
 Website for summer of code in PyJaipur
+***
+
+Hello there, this is the source code for our upcoming launch of the summer of algorithm event :)
--- css/counter-up/jquery.counterup.js
+++ css/counter-up/jquery.counterup.js
@@ -1,11 +1,3 @@
-/*!
-* jquery.counterup.js 1.0
-*
-* Copyright 2013, Benjamin Intal http://gambit.ph @bfintal
-* Released under the GPL v2 License
-*
-* Date: Nov 26, 2013
-*/
 (function( $ ){
   "use strict";
 
