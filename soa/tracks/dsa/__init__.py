e20401ea795df9b67f00f96c6d8533f369bfca1e
arjoonn sharma <arjoonn.94@gmail.com>
2020-04-18 23:23:16 +0530

    site architecture done

--- soa/tracks/dsa/__init__.py
+++ soa/tracks/dsa/__init__.py
@@ -1,11 +1,20 @@
+import bottle
 from soa.tracks.core import Track
 
 
 class DSA(Track):
     slug = "dsa"
-    title = "Data structures and Algorithms"
-    description = "Basic data structures and how they are used in algorithms."
-    is_locked = False
+    title = "Data structures and algorithms"
+    description = "Data structures and how they are used in algorithms."
+
+    @property
+    def is_locked(self):
+        python = Track.trackmap["python"]
+        return (
+            hasattr(bottle.request, "user")
+            and not bottle.request.user.is_.anon
+            and not bottle.request.user.has_completed_track(python)
+        )
 
 
 Track.tracks.append(DSA())
