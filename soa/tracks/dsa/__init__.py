23e86cf9cd51f73eea4674b60061c744eb72717a
arjoonn sharma <arjoonn.94@gmail.com>
2020-04-21 18:16:57 +0530

    lock both tracks on website

--- soa/tracks/dsa/__init__.py
+++ soa/tracks/dsa/__init__.py
@@ -7,13 +7,5 @@ class DSA(Track):
     title = "Data structures and algorithms"
     description = "Data structures and how they are used in algorithms."
     score = 2
+    is_locked = True
     gh_issue_map = {1: 20}
-
-    @property
-    def is_locked(self):
-        python = trackmap["python"]
-        return (
-            hasattr(bottle.request, "user")
-            and not bottle.request.user.is_.anon
-            and not bottle.request.user.has_completed_track(python)
-        )
