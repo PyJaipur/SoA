5311bc216265e16e80ff959f0c5b1012c87e7bc3
Anchal Agarwal <agarwalanchal72@gmail.com>
2020-04-23 19:41:44 +0530

    Added Pyhton track task1 (#26)
    * Added Pyhton track task1
    
    * remove env
    
    * remove env
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Update soa/tracks/python/__init__.py
    
    Co-authored-by: Anchal Agarwal <anchal7299@users.noreply.github.com>
    Co-authored-by: Arjoonn Sharma <theSage21@users.noreply.github.com>

--- soa/__init__.py
+++ soa/__init__.py
@@ -1,5 +1,8 @@
 from gevent import monkey
+import sys
 
+if sys.path[0] != '': #Some redhat distributions are not picking up the current directory in path
+	sys.path.insert(0, '')
 monkey.patch_all()
 from soa import settings
 from soa import models
