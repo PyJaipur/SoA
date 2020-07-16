c167929e9a66630ee2b155850dbaa5e3ca0ed0d2
arjoonn sharma <arjoonn.94@gmail.com>
2020-07-16 23:07:00 +0530

    add ci setup

--- soa/__main__.py
+++ soa/__main__.py
@@ -11,10 +11,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--port", default=8000, type=int)
 parser.add_argument("--tracks-dir", default="tracks", type=str)
 parser.add_argument("--housekeeping", default=None, type=str)
+parser.add_argument("--test-tracks", default=False, action="store_true")
 args = parser.parse_args()
 
 if args.housekeeping:
     housekeeping.handle(args)
+elif args.test_tracks:
+    tracks.core.load_tracks()
 else:
     tracks.core.load_tracks()
     kwargs = {
